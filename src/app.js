ab61089c883dd1ec424d92e170c34c412ae2d368
Durga50 <edurga02@gmail.com>
2025-04-13 15:30:02 +0530

    .

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const todoRouter=require('./routes/todo');
+const app=express();
+const port=3000;
+app.use(express.json());
+app.use('/todo',todoRouter)
+app.listen(port,()=>{
+    console.log(`Hello ${port}`);
+})
